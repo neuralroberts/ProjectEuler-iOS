3f4bba18399f08f61c3623e8c31d4a5d0105f763
Joe Wilkerson <joewilkerson23@gmail.com>
2015-03-20 09:07:02 -0500

    Initial commit from local git

---
+++ ProjectEuler-iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProjectEuler-iOS
+//
+//  Created by Joseph Wilkerson on 3/20/15.
+//  Copyright (c) 2015 Joseph Wilkerson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ProjectEuler-iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProjectEuler-iOS
+//
+//  Created by Joseph Wilkerson on 3/20/15.
+//  Copyright (c) 2015 Joseph Wilkerson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
